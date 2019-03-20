f046842628945375f5f0d6169197651a8ed3d339
mevryn <gelnir000@gmail.com>
2019-03-20 23:41:29 +0100

    main swing window

--- src/main/java/GameEngine/Game.java
+++ src/main/java/GameEngine/Game.java
@@ -1,4 +1,42 @@
-public class Game {
+package GameEngine;
+
+import java.awt.*;
+import GUI.Window;
+
+import javax.swing.text.StyledEditorKit;
+
+public class Game extends Canvas implements Runnable{
+
+    public static final int  WIDTH = 640;
+    public static final int  HEIGHT = 480;
+
+    private Thread thread;
+    private Boolean running = false;
+
+    public Game(){
+        Window window = new Window(WIDTH,HEIGHT,"AiBomber",this);
+    }
+
+    public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        running=true;
+    }
+    public synchronized void stop(){
+        try{
+            thread.join();
+            running=false;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+
     public static void main(String[] args){
+        new Game();
     }
 }
